673d85d0f4282a6776b2e8cdb783aa64dc253cd2
ceruleanox <ceruleanox@gmail.com>
2020-07-04 18:15:56 -0400

    MVP - no misc comments

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ const text = document.querySelector('#text')
 const totalTime = 7500
 const breatheTime = (totalTime / 5) * 2
 const holdTime = totalTime / 5
-const cls = ['theme-1', 'theme-2', 'theme-3']
+
 breatheAnimation()
-changeTheme()
+
 function breatheAnimation() {
     text.innerHTML = 'Breathe In'
     container.className = 'container grow'
@@ -19,21 +19,6 @@ function breatheAnimation() {
             container.className = 'container shrink'
         }, holdTime)
     }, breatheTime)
-}
-
-function changeTheme() {
-    document.getElementById("theme-1-btn").addEventListener('click', function() {
-    document.body.classList.remove(...cls);
-    document.body.classList.add('theme-1');
-  })
-    document.getElementById("theme-2-btn").addEventListener('click', function() {
-      document.body.classList.remove(...cls);
-      document.body.classList.add('theme-2');
-    })
-    document.getElementById("theme-3-btn").addEventListener('click', function() {
-      document.body.classList.remove(...cls);
-      document.body.classList.add('theme-3');
-    })
 }
 
 setInterval(breatheAnimation, totalTime)
