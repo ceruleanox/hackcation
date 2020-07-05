ce8d1ec4f79ebc970a0a679a9f0ef54af2c700a9
jiuu <straisin@gmail.com>
2020-07-04 23:29:27 -0400

    Added Quotes Button
    You can also keep clicking on the button or the quote itself to keep getting different quotes

--- script.js
+++ script.js
@@ -5,40 +5,64 @@ const totalTime = 7500
 const breatheTime = (totalTime / 5) * 2
 const holdTime = totalTime / 5
 const cls = ['theme-1', 'theme-2', 'theme-3']
+var cancel = 0;
 
 breatheAnimation()
-changeTheme()
 function breatheAnimation() {
-    text.innerHTML = 'Breathe In'
+  if (cancel == 0) {
+    document.getElementById("text").innerHTML = 'Breathe In'
     container.className = 'container grow'
 
     setTimeout(() => {
-        text.innerText = 'Hold'
-
+      if (cancel == 0) {
+        document.getElementById("text").innerHTML = 'Hold'
+      }
         setTimeout(() => {
-            text.innerText = 'Breathe Out'
+          if (cancel ==0) {
+
+
+            document.getElementById("text").innerHTML = 'Breathe Out'
             container.className = 'container shrink'
+          }
         }, holdTime)
     }, breatheTime)
+  }
 }
-function changeTheme() {
+function buttons() {
     document.getElementById("theme-1-btn").addEventListener('click', function() {
     document.body.classList.remove(...cls);
     document.body.classList.add('theme-1');
-    document.getElementById("video").src = "darkSkies.mp4"
+    document.getElementById("video").src = "darkSkies.mp4";
 
   })
     document.getElementById("theme-2-btn").addEventListener('click', function() {
       document.body.classList.remove(...cls);
       document.body.classList.add('theme-2');
-      document.getElementById("video").src = "water.mp4"
+      document.getElementById("video").src = "water.mp4";
     })
     document.getElementById("theme-3-btn").addEventListener('click', function() {
       document.body.classList.remove(...cls);
       document.body.classList.add('theme-3');
-      document.getElementById("video").src = "auroraBorealis.mp4"
+      document.getElementById("video").src = "auroraBorealis.mp4";
 
+    })
+    document.getElementById("breathe-btn").addEventListener('click', function() {
+      cancel = 0;
+
+      //quotes[Math.floor(Math.random()*quotes.length)];
+    })
+    document.getElementById("quotes-btn").addEventListener('click', function() {
+      cancel = 1;
+      document.getElementById("text").innerHTML = quotes[Math.floor(Math.random()*quotes.length)];
+
+      //quotes[Math.floor(Math.random()*quotes.length)];
+    })
+    document.getElementById("text").addEventListener('click', function() {
+      if (cancel == 1) {
+        document.getElementById("text").innerHTML = quotes[Math.floor(Math.random()*quotes.length)];
+      }
     })
 }
 
 setInterval(breatheAnimation, totalTime)
+buttons()
